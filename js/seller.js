c7d33ff39fc9b01ae7e10620df15248ca9e50cea
Solomon Raja <91272098+solomonr117@users.noreply.github.com>
2023-03-02 12:27:27 +0530

    Add files via upload

---
+++ js/seller.js
@@ -0,0 +1,19 @@
+
+// ----------------------------------------------------------------
+
+const openModal = document.querySelector('.seller_page_popupdialog');
+
+const btn = document.querySelector('.seller_page_button');
+
+// const seller_page_rmv_btn = document.querySelector('.seller_page_remove_btn');
+
+
+btn.onclick= function(){
+     openModal.style.display="flex";
+   
+}
+
+
+
+
+
